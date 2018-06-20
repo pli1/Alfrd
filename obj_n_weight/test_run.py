ed5659b440f69d7118c822051016506e2ae0cbf1
Pinjia Li <pinjia.li@gmail.com>
2018-06-19 20:28:52 -0700

    Add files via upload

---
+++ obj_n_weight/test_run.py
@@ -0,0 +1,13 @@
+import obj_n_weight
+
+a=obj_n_weight.get_old_items()
+print a
+    
+b=obj_n_weight.get_new_items()
+print b
+obj_n_weight.prepare_old_file()
+a=obj_n_weight.get_old_items()
+print a
+    
+b=obj_n_weight.get_new_items()
+print b
